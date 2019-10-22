7eb24cc354b765d35ab39cb2a1464eed763779ab
Jeremy Harris <cillosis@gmail.com>
2019-10-22 16:47:43 -0400

    Updates

--- web-com-test.e31bb0bc.js
+++ web-com-test.e31bb0bc.js
@@ -794,9 +794,102 @@ function (_WebComponent) {
 }(_webcomponent.default);
 
 _webcomponent.default.addCustomElement('drop-down', DropDown);
+},{"../base/template":"components/base/template.js","../base/webcomponent":"components/base/webcomponent.js"}],"components/ccpa/ccpa.js":[function(require,module,exports) {
+"use strict";
+
+var _template = _interopRequireDefault(require("../base/template"));
+
+var _webcomponent = _interopRequireDefault(require("../base/webcomponent"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+/**
+ * Build Template
+ *
+ * Wrap template build inside a function so we can pass the component context through
+ * @param {WebComponent}
+ */
+var _CCPATemplate = function _CCPATemplate(context) {
+  var options = {};
+  return new _template.default(context).setStyle(
+  /*css*/
+  "\n            *, *:after {\n                box-sizing: border-box;\n            }\n            :host {\n                display: flex;\n                flex-direction: row;\n                justify-content: center;\n                position: fixed;\n                bottom: 0;\n                left: 0;\n                width: 100%;\n                background-color: #e4e4e4;\n                color: #ffffff;\n                padding: 10px;\n                text-align: center;\n            }\n            :host([hidden]) { display: none }\n            .ux-ccpa-footer {\n                display: flex;\n                width: 100%;\n                justify-content: center;\n                line-height: 5rem;\n            }\n            .ux-ccpa-footer__logo {\n                display: flex;\n                justify-content: flex-end;\n                width: 50%;\n                text-align: right;\n                margin-right: 5px;\n            }\n            .ux-ccpa-footer__link {\n                display: flex;\n                justify-content: flex-start;\n                width: 50%;\n                text-align: left;\n                margin-left: 5px;\n            }\n            .ux-ccpa-footer__link a,\n            .ux-ccpa-footer__link a:hover,\n            .ux-ccpa-footer__link a:visited,\n            .ux-ccpa-footer__link a:active {\n                color: #335e7a;\n            }\n        ").setHtml(
+  /*html*/
+  "\n            <div class=\"ux-ccpa-footer\">\n                <div class=\"ux-ccpa-footer__logo\">\n                    <img\n                        src=\"https://www.sos.ca.gov/packages/theme_sos/themes/theme_sos/css/i/seal.png\"\n                        width=\"50px\"\n                        height=\"50px\"\n                    >\n                </div>\n                <div class=\"ux-ccpa-footer__link\">\n                    <a\n                        href=\"https://www.oag.ca.gov/sites/all/files/agweb/pdfs/privacy/ccpa-proposed-regs.pdf\"\n                        target=\"_blank\"\n                    >\n                        Do Not Sell My Personal Information\n                    </a>\n                </div>\n            </div>\n        ");
+};
+
+var CCPAFooter =
+/*#__PURE__*/
+function (_WebComponent) {
+  _inherits(CCPAFooter, _WebComponent);
+
+  _createClass(CCPAFooter, null, [{
+    key: "observedAttributes",
+
+    /**
+     * Attributes MUST be set here in order to use within components onAttributeChanged event
+     */
+    get: function get() {
+      return [];
+    }
+    /**
+     * Initialize Component
+     */
+
+  }]);
+
+  function CCPAFooter() {
+    var _this;
+
+    _classCallCheck(this, CCPAFooter);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(CCPAFooter).call(this));
+
+    _this.setTemplate(_CCPATemplate(_assertThisInitialized(_this)));
+
+    _this.template.render();
+
+    return _this;
+  }
+  /**
+   * On Connect
+   */
+
+
+  _createClass(CCPAFooter, [{
+    key: "onConnect",
+    value: function onConnect() {
+      console.log('[COMPONENT_NOTICE] <ccpa-footer> component connected.');
+    }
+  }]);
+
+  return CCPAFooter;
+}(_webcomponent.default);
+
+_webcomponent.default.addCustomElement('ccpa-footer', CCPAFooter);
 },{"../base/template":"components/base/template.js","../base/webcomponent":"components/base/webcomponent.js"}],"index.js":[function(require,module,exports) {
 require('./components/dropdown/dropdown');
-},{"./components/dropdown/dropdown":"components/dropdown/dropdown.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+
+require('./components/ccpa/ccpa');
+},{"./components/dropdown/dropdown":"components/dropdown/dropdown.js","./components/ccpa/ccpa":"components/ccpa/ccpa.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -824,7 +917,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58603" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58454" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
